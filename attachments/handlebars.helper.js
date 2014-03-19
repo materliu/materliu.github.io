8c624b9ec0bb346b8671db06e5e2c3b5d541ad6f
mater <materliu@gmail.com>
2014-03-19 12:04:05 +0800

    添加 handlebars 模板预编译！

---
+++ attachments/handlebars.helper.js
@@ -0,0 +1,53 @@
+/**
+ * @name             QPlus VM
+ * @namespace        window.qplus
+ * @author
+ * @author
+ * @author
+ * @description      QPlus VM
+ * @requires         N/A
+ * @constructor
+ * @version          1.5.50
+ * @url              http://cdn.qplus.com/js/qplus.api.js
+ * @changelog
+ *                   Ver 0.1.0 @ 14-3-17 Initialize release
+ */
+
+Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
+
+
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
+
+
+
+
